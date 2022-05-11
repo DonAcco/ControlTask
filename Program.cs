2229b0207a7907dd6843ba5d30bf553694f0e6a9
DonAcco <donaccordion@mail.ru>
2022-05-11 16:29:50 +0400

    Finish task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿Console.WriteLine("Введите 5 слов или чисел:");
+string one = Console.ReadLine();
+string two = Console.ReadLine();
+string three = Console.ReadLine();
+string four = Console.ReadLine();
+string five = Console.ReadLine();
+
+string[] startArray = new string[5] {one, two, three, four, five};
+string[] finishArray = new string[startArray.Length];
+int finArrayLen = 0;
+
+void CheckArray(string[] startArray, string[] finishArray)
+{
+    int count = 0;
+    for (int i = 0; i < startArray.Length; i++)
+    {
+    if(startArray[i].Length <= 3)
+        {
+        finishArray[count] = startArray[i];
+        count++;
+        finArrayLen++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < finArrayLen; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Введённые данные с количеством 3 и менее символов:");
+CheckArray(startArray, finishArray);
+PrintArray(finishArray);
